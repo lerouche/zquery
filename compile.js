43c351391fd219c835d46a7d6093593debad0315
Wilson Lin <contact@wilsonl.in>
2016-09-22 23:47:35 +1200

    Updated compiler

--- compile.js
+++ compile.js
@@ -1,12 +1,12 @@
 const fs = require('fs'),
 
-	  SRC_DIR = '../src';
+	  SRC_DIR = __dirname + '/src';
 
 var args = process.argv.slice(2),
 	dest = args.find(arg => /^\-o=.+$/.test(arg)),
 	debug = args.some(arg => /^debug$/.test(arg));
 
-dest = dest ? dest.slice(3) : '.';
+dest = dest ? dest.slice(3) : __dirname + '/dist';
 
 require('zcompile')({
 	src: SRC_DIR,
