48021d926de291af2aa50a0eda352c2df8e776da
Wilson Lin <contact@wilsonl.in>
2018-08-01 20:48:27 +1200

    Use UglifyES directly

--- compile.js
+++ compile.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const Path = require("path");
 const minimist = require("minimist");
+const uglifyes = require("uglify-es");
+const mkdirp = require("mkdirp");
 
 const SRC_DIR = Path.join(__dirname, "src");
 
@@ -9,10 +11,44 @@ const ARGS = minimist(process.argv.slice(2));
 const OUTPUT_DIR = ARGS.out || Path.join(__dirname, "dist");
 const DEBUG = ARGS.debug;
 
-require("zcompile")({
-  src: SRC_DIR,
-  dst: OUTPUT_DIR,
+let code = fs.readFileSync(Path.join(SRC_DIR, "pathquery.js"), "utf8");
 
-  files: fs.readdirSync(SRC_DIR),
-  debug: DEBUG,
+mkdirp.sync(OUTPUT_DIR);
+
+let compressSettings = {
+  booleans: true,
+  collapse_vars: true,
+  comparisons: true,
+  conditionals: true,
+  dead_code: true,
+  drop_console: !DEBUG,
+  drop_debugger: !DEBUG,
+  evaluate: true,
+  hoist_funs: true,
+  hoist_vars: false,
+  if_return: true,
+  join_vars: true,
+  keep_fargs: false,
+  keep_fnames: false,
+  loops: true,
+  negate_iife: true,
+  properties: true,
+  reduce_vars: true,
+  sequences: true,
+  unsafe: true,
+  unused: true,
+  // Return compressor warnings in result.warnings
+  warnings: true,
+};
+
+let result = uglifyes.minify(code, {
+  parse: {
+    shebang: true,
+    bare_returns: true,
+  },
+  warnings: false,
+  mangle: true,
+  compress: compressSettings,
 });
+
+fs.writeFileSync(Path.join(OUTPUT_DIR, "pathquery.js"), result.code);
